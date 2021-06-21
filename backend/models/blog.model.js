42104652372d1744fc665485cb7653ebc05bb781
Ulysses Milton <ulysses.github@protonmail.com>
2021-06-21 00:21:27 -0500

    Started adding backend for blog

---
+++ backend/models/blog.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    }
+}, {
+    timestamps: true,
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = Blog;
---
+++ backend/routes/blog.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+let Blog = require('../models/blog.model');
+
+router.route('/').get((req, res) =>{
+    Blog.find()
+        .then(blogposts => res.json(blogposts))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+const title = req.body.title;
+const body = req.body.body;
+const date = Date.parse(req.body.date);
+
+const newBlogpost = new Blog({
+    title,
+    body,
+    date,
+});
+
+newBlogpost.save()
+.then(() => res.json('New post added!'))
+.catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
